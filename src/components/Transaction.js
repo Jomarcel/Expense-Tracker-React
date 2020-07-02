fd1f8c86fb30bd912ea9b9cb14f4b784006d919a
Jomarcel <jomarcel_n@outlook.com>
2020-07-03 02:38:13 +1000

    if the transaction amount is a positive number, show a plus sign, otherwise it's a negative number, show a minus sign

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
-  console.log(transaction);
+  //   console.log(transaction);
 
   return (
-    <li className="minus">
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {/*         Math.abs is used here to transform transction.amount to positive number and to remove any negative sign */}
