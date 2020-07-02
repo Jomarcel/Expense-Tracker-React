86fb0088891039895788a3b8d25057adb244b0da
Jomarcel <jomarcel_n@outlook.com>
2020-07-03 02:35:21 +1000

    Run the reduce method on the transaction array to calculate tthe total income

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+  // console.log(amounts);
+  const total = amounts
+    .reduce((accumulator, currentValue) => (accumulator += currentValue), 0)
+    .toFixed(2);
+  console.log(total);
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
