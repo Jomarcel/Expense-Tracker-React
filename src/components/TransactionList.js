e44493ca129e19b28e73b45d7fa4651e525fda6a
Jomarcel <jomarcel_n@outlook.com>
2020-07-03 01:22:04 +1000

    Added a list of transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,18 +1,19 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import Transaction from "../components/Transaction";
 
 const TransactionList = () => {
   // pass the transactions data
-  const context = useContext(GlobalContext);
-  console.log(context);
+  const { transactions } = useContext(GlobalContext);
+  console.log(transactions);
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          // console.log(transaction)
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </div>
   );
