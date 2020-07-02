eb87bb698c7bae6c1647e79c8f989ccdb10f4d84
Jomarcel <jomarcel_n@outlook.com>
2020-07-03 01:18:34 +1000

    added transaction componenet

---
+++ src/components/Transaction.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  console.log(transaction);
+
+  return (
+    <li className="minus">
+      {transaction.text}{" "}
+      <span>
+        {/*         Math.abs is used here to transform transction.amount to positive number and to remove any negative sign */}
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
+
+export default Transaction;
