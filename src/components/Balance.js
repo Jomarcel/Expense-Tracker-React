16359a394231cf47dceae2981e8c8ec3805abf8a
Jomarcel <jomarcel_n@outlook.com>
2020-07-05 17:00:22 +1000

    adding up the transaction totals

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,11 +4,10 @@ import { GlobalContext } from "../context/GlobalState";
 const Balance = () => {
   const { transactions } = useContext(GlobalContext);
   const amounts = transactions.map((transaction) => transaction.amount);
-  console.log(amounts);
   const total = amounts
     .reduce((accumulator, currentValue) => (accumulator += currentValue), 0)
     .toFixed(2);
-  console.log(total);
+
   return (
     <div>
       <h4>Your Balance</h4>
